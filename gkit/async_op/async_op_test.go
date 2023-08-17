0c03f0a8f2fec9f82e79ab09075ff3ca0cbe1490
cr-mao <crmao@qq.com>
2023-08-17 15:53:12 +0800

    游戏可用的一些组件库

---
+++ gkit/async_op/async_op_test.go
@@ -0,0 +1,43 @@
+package async_op_test
+
+import (
+	"fmt"
+	"github.com/cr-mao/loric/gkit/async_op"
+	"testing"
+	"time"
+)
+
+// LoginByPasswordAsync 根据用户名称和密码进行登录
+func LoginByPasswordAsync(userName string, password string) *async_op.AsyncBizResult {
+	if len(userName) <= 0 ||
+		len(password) <= 0 {
+		return nil
+	}
+
+	bizResult := &async_op.AsyncBizResult{}
+	bindId := async_op.StrToWorkId(userName)
+	async_op.Process(bindId, func() {
+		// 模拟获得用户数据
+		bizResult.SetReturnedObj("user_result")
+	}, nil)
+
+	return bizResult
+}
+
+func TestAsyncBizResult(t *testing.T) {
+	// 根据用户名称和密码登录
+	res := LoginByPasswordAsync("crmao", "123456")
+	time.Sleep(time.Second * 3)
+
+	res.OnComplete(func() {
+		returnedObj := res.GetReturnedObj()
+		if nil == returnedObj {
+			fmt.Println("err nil ")
+			return
+		}
+		user := returnedObj.(string)
+		fmt.Println(111, user)
+	})
+
+	time.Sleep(time.Second * 5)
+}
---
+++ gkit/lazy_save/lazy_save_test.go
@@ -0,0 +1,80 @@
+package lazy_save_test
+
+import (
+	"fmt"
+	"github.com/cr-mao/loric/gkit/lazy_save"
+	"testing"
+	"time"
+)
+
+type User struct {
+	UserId   int64 //用户id
+	Blood    int64 //血量
+	Capacity int64 //能量
+}
+
+type UserLazySaveObj struct {
+	*User
+}
+
+func (user *UserLazySaveObj) GetLsoId() string {
+	return fmt.Sprintf("userdata_%v", user.UserId)
+}
+
+func (user *UserLazySaveObj) SaveOrUpdate(callback func()) {
+
+	fmt.Println("执行存库操作，可能是异步,当前时间戳:", time.Now().Unix())
+
+	if callback != nil {
+		callback()
+	}
+}
+
+/*
+=== RUN   TestLazySave
+begin 1690676226
+INFO msg=记录延迟保存数据, lsoId = userdata_1
+INFO msg=记录延迟保存数据, lsoId = userdata_1
+INFO msg=记录延迟保存数据, lsoId = userdata_1
+INFO msg=记录延迟保存数据, lsoId = userdata_1
+INFO msg=记录延迟保存数据, lsoId = userdata_1
+INFO msg=记录延迟保存数据, lsoId = userdata_1
+INFO msg=记录延迟保存数据, lsoId = userdata_1
+INFO msg=记录延迟保存数据, lsoId = userdata_1
+INFO msg=记录延迟保存数据, lsoId = userdata_1
+INFO msg=记录延迟保存数据, lsoId = userdata_1
+INFO msg=记录延迟保存数据, lsoId = userdata_1
+INFO msg=执行延迟保存, lsoId = userdata_1
+执行存库操作，可能是异步,当前时间戳: 1690676257
+*/
+func TestLazySave(t *testing.T) {
+	t.SkipNow()
+	// 模拟高频场景
+	times := 0
+	fmt.Println("begin", time.Now().Unix())
+	for {
+		time.Sleep(1 * time.Second)
+		var user = &User{
+			1,
+			10000,
+			20000,
+		}
+		// 延迟保存用户数据
+		lazySaveObj := &UserLazySaveObj{
+			User: user,
+		}
+		// 20秒后才会更改
+		lazy_save.SaveOrUpdate(lazySaveObj)
+
+		// 手动保存
+		lazySaveObj.SaveOrUpdate(func() {
+			fmt.Println("手动保存成功")
+		})
+
+		times++
+		if times > 3 {
+			break
+		}
+	}
+	time.Sleep(20 * time.Second)
+}
